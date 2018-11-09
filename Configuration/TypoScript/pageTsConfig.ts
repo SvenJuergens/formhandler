72e35877bc74943678707b90a35aa87af3709290
SvenJuergens <sj@nordsonne.de>
2018-11-09 18:22:19 +0100

    [TASK] updated icon

--- Configuration/TypoScript/pageTsConfig.ts
+++ Configuration/TypoScript/pageTsConfig.ts
@@ -1,7 +1,7 @@
 mod.wizards.newContentElement.wizardItems.forms {
 	elements {
 		formhandler {
-			icon = ../typo3conf/ext/formhandler/ext_icon.gif
+			icon = ../typo3conf/ext/formhandler/Resources/Public/Icons/Extension.png
 			title = Formhandler
 			description = The swiss army knife for all kinds of mailforms.
 			tt_content_defValues.CType = formhandler_pi1

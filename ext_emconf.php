2c30511324f8f3972faa8ebaab4f8580e52e4759
SvenJuergens <sj@nordsonne.de>
2019-05-04 11:52:49 +0200

    [TASK] add dependency to typo3db_legacy

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,6 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '8.7.0-9.5.99',
+            'typo3db_legacy' => '1.1.1-1.2.0'
         ],
         'conflicts' => [],
         'suggests' => [],
